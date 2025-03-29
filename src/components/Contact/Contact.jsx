3ea9ef466e433d1d02e25f06ce4cdaf12c13bd34
Kazi Amin <126922373+KaziAmin110@users.noreply.github.com>
2025-03-29 10:45:41 -0400

    Added HTML Changes

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -6,23 +6,23 @@ export const Contact = () => {
   return (
     <footer id="contact" className={styles.container}>
       <div className={styles.text}>
-        <h2>Contact</h2>
+        <h2>Contact Me</h2>
         <p>Feel free to reach out!</p>
       </div>
       <ul className={styles.links}>
         <li className={styles.link}>
           <img src={`/assets/contact/emailIcon.png`} alt="Email Icon" />
-          <a href="mailto:myemail@email.com">myemail@email.com</a>
+          <a href="mailto:kaziamin1239@gmail.com">Email</a>
         </li>
 
         <li className={styles.link}>
           <img src={`/assets/contact/linkedinIcon.png`} alt="Linkedin Icon" />
-          <a href="mailto:myemail@email.com">My Linkedin</a>
+          <a href="https://www.linkedin.com/in/kazi-amin/">Linkedin</a>
         </li>
 
         <li className={styles.link}>
           <img src={`/assets/contact/githubIcon.png`} alt="Github Icon" />
-          <a href="mailto:myemail@email.com">My Github</a>
+          <a href="https://github.com/KaziAmin110">Github</a>
         </li>
       </ul>
     </footer>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ export const Navbar = () => {
   return (
     <nav className={styles.navbar}>
       <a href="/" className={styles.title}>
-        Portfolio
+        My Portfolio
       </a>
       <div className={styles.menu}>
         <img
